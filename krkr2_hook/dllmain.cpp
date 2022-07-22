9dd7b8314a4ec19b46fa1cc80e16470d8f44ec0f
Jerry Shen <47900658+AyamiKaze@users.noreply.github.com>
2022-07-22 23:36:49 +0800

    Add files via upload

---
+++ krkr2_hook/dllmain.cpp
@@ -0,0 +1,215 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+// Base AQUA [SORAHANE]
+#include "framework.h"
+#include "tp_stub.h"
+#include "compact_enc_det/compact_enc_det.h"
+
+// 早期kr2会出现问题，打开这个hook可以解决。
+auto pfnGetProcAddress = GetProcAddress;
+FARPROC WINAPI HookGetProcAddress(HMODULE hModule, LPCSTR lpProcName) {
+    if (!strcmp(lpProcName, "GetSystemWow64DirectoryA"))
+        return NULL;
+    return pfnGetProcAddress(hModule, lpProcName);
+}
+
+// 不稳定，遇到问题建议单独测试处理
+auto pfnMultiByteToWideChar = MultiByteToWideChar;
+int WINAPI HookMultiByteToWideChar(UINT cp, DWORD dwFg, LPCSTR lpMBS, int cbMB, LPWSTR lpWCS, int ccWC){
+
+    bool is_reliable;
+    int bytes_consumed;
+    Encoding encoding = CompactEncDet::DetectEncoding(lpMBS, strlen(lpMBS), nullptr, nullptr, nullptr, UNKNOWN_ENCODING, UNKNOWN_LANGUAGE, CompactEncDet::QUERY_CORPUS, true, &bytes_consumed, &is_reliable);
+    UINT codepage = cp;
+    if (encoding == JAPANESE_SHIFT_JIS && is_reliable){
+        codepage = 932;
+    }
+    return pfnMultiByteToWideChar(codepage, dwFg, lpMBS, cbMB, lpWCS, ccWC);
+}
+
+BOOL APIHook(){
+    /*
+    if (!Hook(pfnGetProcAddress, HookGetProcAddress)) {
+        E(L"Hook GetProcAddress Error");
+        return FALSE;
+    }
+    */
+    ///*
+    if (!Hook(pfnMultiByteToWideChar, HookMultiByteToWideChar)) {
+        E(L"Hook MultiByteToWideChar Error");
+        return FALSE;
+    }
+    //*/
+    return TRUE;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// pfnTVPCreateStream搜特征码 55 8B EC 81 C4 ?? ?? ?? ?? 53 56 57 89 95 ?? ?? ?? ?? 89 85 ?? ?? ?? ?? B8 ?? ?? ?? ?? C7 85 ?? ?? ?? ?? ?? ?? ?? ?? 89 65 80 89 85 ?? ?? ?? ?? 66 C7 45 ?? ?? ?? 33 D2 89 55 90 64 8B 0D ?? ?? ?? ?? 89 8D ?? ?? ?? ?? 8D 85 ?? ?? ?? ?? 64 A3 ?? ?? ?? ?? 66 C7 45 ?? ?? ?? 8B 95 ?? ?? ?? ?? 8B 85 ?? ?? ?? ?? E8 ?? ?? ?? ?? 8B 95 ?? ?? ?? ?? 64 89 15 ?? ?? ?? ?? E9 ?? ?? ?? ?? 6A 23 8B 8D ?? ?? ?? ?? 83 39 00 74 20 8B 85 ?? ?? ?? ?? 8B 00 85 C0 75 04 33 D2
+// TVPFunctionExporter地址在TVPGetFunctionExporter里面，return的地址。
+
+PVOID pfnTVPCreateStream = (PVOID)(BaseAddr + 0x1FFB3C);
+iTVPFunctionExporter* TVPFunctionExporter = (iTVPFunctionExporter*)(BaseAddr + 0x2F98E4);
+BOOL inited = FALSE;
+
+class tTJSCriticalSection{
+    CRITICAL_SECTION CS;
+public:
+    tTJSCriticalSection() { InitializeCriticalSection(&CS); }
+    ~tTJSCriticalSection() { DeleteCriticalSection(&CS); }
+
+    void Enter() { EnterCriticalSection(&CS); }
+    void Leave() { LeaveCriticalSection(&CS); }
+};
+
+class tTJSCriticalSectionHolder{
+    tTJSCriticalSection* Section;
+public:
+    tTJSCriticalSectionHolder(tTJSCriticalSection& cs){
+        Section = &cs;
+        Section->Enter();
+    }
+
+    ~tTJSCriticalSectionHolder(){
+        Section->Leave();
+    }
+
+};
+
+static tTJSCriticalSection LocalCreateStreamCS;
+
+
+std::wstring GetKrkrFileName(LPCWSTR Name){
+    std::wstring Info(Name);
+
+    if (Info.find_last_of(L">") != std::wstring::npos)
+        Info = Info.substr(Info.find_last_of(L">") + 1, std::wstring::npos);
+
+    if (Info.find_last_of(L"/") != std::wstring::npos)
+        Info = Info.substr(Info.find_last_of(L"/") + 1, std::wstring::npos);
+
+    return Info;
+}
+
+void FileNameToLower(std::wstring& FileName){
+    std::transform(FileName.begin(), FileName.end(), FileName.begin(), ::tolower);
+}
+
+const tjs_char* TJSStringGetPtr(tTJSString* s){
+    if (!s)
+        return L"";
+
+    tTJSVariantString_S* v = *(tTJSVariantString_S**)s;
+
+    if (!v)
+        return L"";
+
+    if (v->LongString)
+        return v->LongString;
+
+    return v->ShortString;
+}
+
+IStream* CreateLocalStream(LPCWSTR FileName) {
+
+    tTJSCriticalSectionHolder CSHolder(LocalCreateStreamCS);
+
+    wstring fnm = GetKrkrFileName(FileName);
+    FileNameToLower(fnm);
+    wstring NewFileName = L"#Project\\" + fnm;
+
+    IStream* pStream;
+    auto hr = SHCreateStreamOnFileEx(NewFileName.c_str(), STGM_READ, 0, FALSE, NULL, &pStream);
+    if (SUCCEEDED(hr)) {
+        //cout << "Replace:" << wtoc(NewFileName.c_str(), 936) << endl;
+        return pStream;
+    }
+    else {
+        return NULL;
+    }
+}
+
+_declspec(naked) tTJSBinaryStream* TVPCreateStreamCallback(ttstr* name, tjs_uint32 flags) {
+    _asm {
+        mov edx, flags;
+        mov eax, name;
+        call pfnTVPCreateStream;
+        ret;
+    }
+}
+
+tTJSBinaryStream* _HookTVPCreateStream(ttstr* name, tjs_uint32 flags) {
+    tTJSBinaryStream* Stream;
+    IStream* IStream;
+
+    if (!inited) {
+        TVPInitImportStub(TVPFunctionExporter);
+        cout << "func list: 0x" << hex << (DWORD)TVPFunctionExporter << endl;
+        inited = TRUE;
+    }
+
+    if (flags == TJS_BS_READ) {
+        const tjs_char* psz = TJSStringGetPtr(name);
+        wstring fnm(psz);
+        IStream = CreateLocalStream(fnm.c_str());
+
+        if (IStream) {
+            Stream = TVPCreateBinaryStreamAdapter(IStream);
+            if (!Stream) {
+                E(L"TVPCreateBinaryStreamAdapter Error.");
+                Stream = TVPCreateStreamCallback(name, flags);
+            }
+            else {
+                cout << "Replace:" << wtoc(fnm.c_str(), 936) << endl;
+            }
+        }
+        else {
+            Stream = TVPCreateStreamCallback(name, flags);
+        }
+    }
+    else {
+        Stream = TVPCreateStreamCallback(name, flags);
+    }
+
+    return Stream;
+}
+
+_declspec(naked) void HookTVPCreateStream() {
+    _asm {
+        push edx;
+        push eax;
+        call _HookTVPCreateStream;
+        add esp, 8;
+        ret;
+    }
+}
+
+BOOL TVPHook(){
+    DetourUpdateThread(GetCurrentThread());
+    DetourTransactionBegin();
+    DetourAttach(&pfnTVPCreateStream, HookTVPCreateStream);
+    if (DetourTransactionCommit() != NOERROR) {
+        E(L"TVP Hook Error.");
+        return FALSE;
+    }
+    return TRUE;
+}
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        make_console();
+        if (!APIHook() || !TVPHook()) {
+            E(L"Hook Error.");
+            ExitProcess(-1);
+        }
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ krkr2_hook/framework.h
@@ -0,0 +1,188 @@
+﻿// header.h: 标准系统包含文件的包含文件，
+// 或特定于项目的包含文件
+//
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
+// Windows 头文件
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm> 
+#include "shlwapi.h"
+#include <unordered_map>
+//#include <map>
+#include <intrin.h>
+
+#include "detours.h"
+
+#pragma comment(lib, "Shlwapi.lib")
+using namespace std;
+
+HMODULE Base = GetModuleHandle(NULL);
+DWORD BaseAddr = (DWORD)Base;
+
+static void make_console() {
+	AllocConsole();
+	freopen("CONOUT$", "w", stdout);
+	freopen("CONIN$", "r", stdin);
+	std::cout << "Open Console Success!" << std::endl;
+}
+
+static BOOL IATPatch(LPCSTR szDllName, PROC pfnOrg, PROC pfnNew)
+{
+	HMODULE hmod;
+	LPCSTR szLibName;
+	PIMAGE_IMPORT_DESCRIPTOR pImportDesc;
+	PIMAGE_THUNK_DATA pThunk;
+	DWORD dwOldProtect, dwRVA;
+	PBYTE pAddr;
+
+	hmod = GetModuleHandleW(NULL);
+	pAddr = (PBYTE)hmod;
+	pAddr += *((DWORD*)&pAddr[0x3C]);
+	dwRVA = *((DWORD*)&pAddr[0x80]);
+
+	pImportDesc = (PIMAGE_IMPORT_DESCRIPTOR)((DWORD)hmod + dwRVA);
+
+	for (; pImportDesc->Name; pImportDesc++)
+	{
+		szLibName = (LPCSTR)((DWORD)hmod + pImportDesc->Name);
+		if (!stricmp(szLibName, szDllName))
+		{
+			pThunk = (PIMAGE_THUNK_DATA)((DWORD)hmod + pImportDesc->FirstThunk);
+			for (; pThunk->u1.Function; pThunk++)
+			{
+				if (pThunk->u1.Function == (DWORD)pfnOrg)
+				{
+					VirtualProtect((LPVOID)&pThunk->u1.Function, 4, PAGE_EXECUTE_READWRITE, &dwOldProtect);
+					pThunk->u1.Function = (DWORD)pfnNew;
+					VirtualProtect((LPVOID)&pThunk->u1.Function, 4, dwOldProtect, &dwOldProtect);
+					return TRUE;
+				}
+			}
+		}
+	}
+	return FALSE;
+}
+
+template<class T>
+BOOL Hook(T& OriginalFunction, T DetourFunction)
+{
+	DetourUpdateThread(GetCurrentThread());
+	DetourTransactionBegin();
+	DetourAttach(&(PVOID&)OriginalFunction, (PVOID&)DetourFunction);
+	if (DetourTransactionCommit() != NOERROR)
+		return FALSE;
+	return TRUE;
+}
+
+
+template<class T>
+BOOL UnHook(T& OriginalFunction, T DetourFunction)
+{
+	DetourUpdateThread(GetCurrentThread());
+	DetourTransactionBegin();
+	DetourDetach(&(PVOID&)OriginalFunction, (PVOID&)DetourFunction);
+	if (DetourTransactionCommit() != NOERROR)
+		return FALSE;
+	return TRUE;
+}
+
+void ErrorToFileW(PVOID buffer, DWORD size)
+{
+	FILE* fp = nullptr;
+	auto err = fopen_s(&fp, "LogErrorW.txt", "ab+");
+	if (!fp)
+	{
+		MessageBoxW(0, L"Create error file error.", L"ErrorToFileW", MB_OK | MB_ICONERROR);
+		ExitProcess(0);
+	}
+	fwrite(buffer, size, 1, fp);
+	fwrite(L"\r\n", 4, 1, fp);
+	fclose(fp);
+}
+
+void E(const wchar_t* format, ...)
+{
+	wchar_t buffer[0x1000];
+	va_list ap;
+	va_start(ap, format);
+	auto size = vswprintf_s(buffer, format, ap);
+	std::wcout << buffer << std::endl;
+	MessageBoxW(0, buffer, L"Error", MB_OK | MB_ICONERROR);
+	ErrorToFileW(buffer, size * 2);
+	va_end(ap);
+}
+
+static char* wtoc(LPCTSTR str, UINT cp)
+{
+	DWORD dwMinSize;
+	dwMinSize = WideCharToMultiByte(cp, NULL, str, -1, NULL, 0, NULL, FALSE); //计算长度
+	char* out = new char[dwMinSize];
+	WideCharToMultiByte(cp, NULL, str, -1, out, dwMinSize, NULL, FALSE);//转换
+	return out;
+}
+
+static LPWSTR ctow(char* str, UINT cp)
+{
+	DWORD dwMinSize;
+	dwMinSize = MultiByteToWideChar(cp, 0, str, -1, NULL, 0); //计算长度
+	LPWSTR out = new wchar_t[dwMinSize];
+	MultiByteToWideChar(cp, 0, str, -1, out, dwMinSize);//转换
+	return out;
+}
+
+static void BMP_TO_DIB(PBYTE data, int width, int height, int BitCount)
+{
+	BYTE* TempBuffer = nullptr;
+	int i = 0;
+	int widthlen = 0;
+	int nAlignWidth = 0;
+	size_t BufferSize = 0;
+	nAlignWidth = (width * 32 + 31) / 32;
+	BufferSize = 4 * nAlignWidth * height;
+	TempBuffer = (BYTE*)malloc(BufferSize);
+
+	//反转图片,修正图片信息
+	widthlen = width * (BitCount / 8); //对齐宽度大小
+	for (i = 0; i < height; i++) {
+		memcpy(&TempBuffer[(((height - i) - 1) * widthlen)], &data[widthlen * i], widthlen);
+	}
+
+	memcpy(data, TempBuffer, BufferSize);
+
+	free(TempBuffer);
+}
+
+static string ReplaceString(string& line)
+{
+	string out;
+	for (int i = 0; i < line.size(); i++)
+	{
+		if (line[i] == '\\')
+		{
+			i++;
+			if (line[i] == 'r')
+				out += '\r';
+			else if (line[i] == 'n')
+				out += '\n';
+			else
+				out += line[i];
+		}
+		else
+			out += line[i];
+	}
+	return out;
+}
+
+BOOL IsStr(const CHAR* str){
+	for (int i = 0; i < strlen(str);i++) {
+		if (str[i] < 0x20)
+			return FALSE;
+	}
+	return TRUE;
+}
